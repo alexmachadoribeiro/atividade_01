ab6ae760f7c8699f55656e832268b442f0597f0e
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-09-24 20:32:54 -0300

    Versão 1.0.

---
+++ app.py
@@ -0,0 +1,36 @@
+'''
+Crie um programa que receba do usuário as seguintes informações:
+- Nome
+- CPF
+- Telefone
+- E-mail
+- Endereço
+- Gênero
+- Escolaridade
+- Signo
+- Tipo Sanguíneo
+Ao final, o programa exibe essas informações na tela.
+Ao terminar, crie um repositório no GitHub.
+'''
+
+# entrada de dados
+nome = input("Informe o nome: ")
+cpf = input("Informe o seu CPF: ")
+telefone = input("Informe o telefone: ")
+email = input("Informe o e-mail: ")
+endereco = input("Informe o endereço: ")
+genero = input("Informe o gênero: ")
+escolaridade = input("Informe sua escolaridade: ")
+signo = input("Informe seu signo: ")
+tipo_sanguineo = input("Informe o seu tipo sanguíneo: ")
+
+# saída de dados
+print(f"Nome: {nome}.")
+print(f"CPF: {cpf}.")
+print(f"Telefone: {telefone}.")
+print(f"E-mail: {email}.")
+print(f"Endereço: {endereco}.")
+print(f"Gênero: {genero}.")
+print(f"Escolaridade: {escolaridade}.")
+print(f"Signo: {signo}.")
+print(f"Tipo sanguíneo: {tipo_sanguineo}.")
